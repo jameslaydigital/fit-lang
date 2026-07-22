679f256795a3c871649feae9841d71982fca2c01
james lay <jameslaymusic@gmail.com>
2026-07-22 11:15:41 -0600

    begin workouts, use ids

--- modules/plans/main.js
+++ modules/plans/main.js
@@ -3,47 +3,25 @@ import { cmds } from "../../cmds.js";
 
 data.plans = data.plans ?? [];
 
-class Plan {
-    constructor(name) {
-        if (typeof name !== "string") {
-            throw new Error("plan name must be a string");
-        }
-        if (name === "") {
-            throw new Error("plan name is empty");
-        }
-        this.name = name;
-    }
-    static fromObject(obj) {
-        if (typeof obj !== "object" || obj === null) {
-            throw new Error(
-                "Plan::fromObject expects something other than " +
-                JSON.stringify(obj)
-            );
-        }
-        if (typeof obj.name !== "string") {
-            throw new Error("cannot construct plan because it's missing a name!");
-        }
-        return new Plan(obj.name);
-    }
-}
-
 // plan CRUD
 cmds.register("plan", async function() {
+    const routes = cmds.getRoutes("plan ");
     console.log("plan - workout plans");
     console.log("--------------------");
     console.log("options:");
-    console.log("\tplan list");
-    console.log("\tplan rename");
+    for (const route of routes) {
+        console.log("\t%s", route);
+    }
 });
 
-cmds.register("stack push plan", async function([plan_name]) {
-    if (typeof plan_name !== "string") {
-        console.error("cannot push plan without plan name");
-        console.log("usage: stack push plan <plan_name>");
+cmds.register("stack push plan", async function([id]) {
+    if (typeof id !== "string") {
+        console.error("cannot push plan without plan id");
+        console.log("usage: stack push plan <id>");
         return;
     }
 
-    const plan = data.plans.find(p => p.name === plan_name);
+    const plan = data.plans.find(p => p.id === id);
     if (plan) {
         stack.push(plan);
         await save();
@@ -61,7 +39,14 @@ cmds.register("plan unchoose", async function() {
 });
 
 cmds.register("plan list", async function() {
-    console.log(data.plans);
+    console.log("plans:");
+    if (data.plans.length === 0) {
+        console.log("\tNo plans. Add one with `plan create <name>`");
+        return;
+    }
+    for (const plan of data.plans) {
+        console.log("\t- %s", JSON.stringify(plan));
+    }
 });
 
 cmds.register("plan create", async function([name]) {
@@ -75,19 +60,29 @@ cmds.register("plan create", async function([name]) {
         console.log("usage: plan create <plan_name>");
         return;
     }
-    data.plans.push(new Plan(name));
+    data.plans.push({
+        type: "plan",
+        id: Math.random(36).toString().split(".").pop(),
+        name: name,
+    });
     save();
     console.log("created new plan '%s'", name);
 });
-cmds.register("plan rename", async function([oldname, newname]) {
-    if (!oldname || !newname) {
+
+cmds.register("plan rename", async function([id, newname]) {
+    const usage = "usage:\n\tfit plan rename <id> <newname>";
+    if (!id) {
+        console.log("fit plan rename: missing id");
+        console.log(usage);
+        return;
+    }
+    if (!newname) {
         console.log("fit plan rename: missing a name");
-        console.log("usage:");
-        console.log("\tfit plan rename <oldname> <newname>");
+        console.log(usage);
         return;
     }
 
-    const plan = data.plans.find(p => p.name === oldname);
+    const plan = data.plans.find(p => p.id === id);
     if (!plan) {
         console.log("no plan found by name '%s'", oldname);
         return;
--- modules/workouts/main.js
+++ modules/workouts/main.js
@@ -1,17 +1,102 @@
+import { data, stack, save } from "../../storage.js";
+import { cmds } from "../../cmds.js";
 
-class Workout {
-    constructor(name) {
-        if (typeof name !== "string") {
-            throw new Error("workout name must be a string");
-        }
-        if (name === "") {
-            throw new Error("workout name is empty");
-        }
-        this.name = name;
-        this.plan = null;
+data.workouts = data.workouts ?? {};
+
+cmds.register("workout", function() {
+    console.log("workout - a series of exercises");
+    console.log("-------------------------------");
+    console.log("options:");
+    const routes = cmds.getRoutes("workout ");
+    for (const route of routes) {
+        console.log("\t%s", route);
+    }
+});
+
+cmds.register("workout list", async function() {
+    console.log("workouts:");
+    if (data.workouts.length === 0) {
+        console.log("no workouts. Add one with `workout create`");
+        return;
+    }
+    for (const workout of data.workouts) {
+        console.log("\t- %s", JSON.stringify(workout));
+    }
+});
+
+cmds.register("workout create", async function([name]) {
+    if (typeof name !== "string" || name === "") {
+        console.error("error: name required");
+        console.log("usage: workout create <name>");
+        return;
     }
 
-    setPlan(plan) {
-        this.plan = plan;
+    data.workouts.push({
+        type: "workout",
+        id: Math.random(36).toString().split(".").pop(),
+        name: name,
+        plan: null,
+    });
+
+    await save();
+    console.log("workout '%s' created", name);
+});
+
+cmds.register("workout remove", async function([name]) {
+    if (typeof name !== "string" || name === "") {
+        console.error("error: name required");
+        console.log("usage: workout remove <name>");
+        return;
     }
-}
+    const length = data.workouts.length;
+    data.workouts = data.workouts.filter(w => w.name !== name);
+    await save();
+    const diff = length - data.workouts.length;
+    if (diff === 0) {
+        console.log("no workouts removed");
+    } else {
+        console.log("%d workout(s) removed", diff);
+    }
+});
+
+cmds.register("workout read", async function([name]) {
+    if (typeof name !== "string" || name === "") {
+        console.error("error: name required");
+        console.log("usage: workout read <name>");
+        return;
+    }
+
+    const workout = data.workouts.find(w => w.name === name);
+    if (!workout) {
+        console.log("no workout found by name '%s'", name);
+        return;
+    }
+    console.log("%s", JSON.stringify(workout, null, 4));
+});
+
+cmds.register("workout rename", async function([oldName, newName]) {
+    if (typeof oldName !== "string" || oldName === "") {
+        console.error("error: old name required");
+        console.log("usage: workout rename <old-name> <new-name>");
+        return;
+    }
+    if (typeof newName !== "string" || newName === "") {
+        console.error("error: new name required");
+        console.log("usage: workout rename <old-name> <new-name>");
+        return;
+    }
+    
+    const workout = data.workouts.find(w => w.name === oldName);
+    if (!workout) {
+        console.error("error: workout '%s' does not exist", oldName);
+        return;
+    }
+    
+    workout.name = newName;
+    await save();
+    console.log("workout '%s' renamed to '%s'", oldName, newName);
+});
+
+cmds.register("plan add workout", async function([name]) {
+    // okay this one takes some thinking...
+});
