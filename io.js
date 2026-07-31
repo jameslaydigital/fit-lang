e75f4c71a85f67451aad4111febe91360723b46b
james lay <jameslaymusic@gmail.com>
2026-07-31 10:07:10 -0600

    update render

--- io.js
+++ io.js
@@ -1,6 +1,7 @@
 let impl = {
     print: (...args) => console.log(...args),
     error: (...args) => console.error(...args),
+    render: (html) => console.log(html),
     exit: () => {},
 };
 
@@ -10,4 +11,5 @@ export function setOutput(output) {
 
 export function print(...args) { impl.print(...args); }
 export function error(...args) { impl.error(...args); }
+export function render(...args) { impl.render(...args); }
 export function exit(code) { impl.exit(code); }
